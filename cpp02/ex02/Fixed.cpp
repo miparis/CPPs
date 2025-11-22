8975ffaa07c9472e03f819512e5a4e461940b258
Miranda Paris <miparis@c2r3s3.42madrid.com>
2025-11-22 19:05:41 +0100

    ex02 done

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -6,34 +6,28 @@
 /*   By: miparis <miparis@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 16:04:38 by miparis           #+#    #+#             */
-/*   Updated: 2025/11/22 16:23:12 by miparis          ###   ########.fr       */
+/*   Updated: 2025/11/22 19:01:20 by miparis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed::Fixed(void) : _fixed(0)
-{
-	//std::cout << GREEN << "Default constructor called" << NC << std::endl;
+Fixed::Fixed(void) : _fixed(0){
 }
 
 Fixed::Fixed(const Fixed& _new)
 {
-	//std::cout << BLUE << "Copy constructor called" << NC << std::endl;
 	*this = _new;
 }
 
 Fixed &Fixed::operator=(const Fixed& _new)
 {
-	//std::cout << PURPLE << "Copy assignment operator called" << NC << std::endl;
 	if (this != &_new )
 		this->_fixed = _new.getRawBits();
 	return (*this);
 }
 
-Fixed:: ~Fixed(void)
-{
-	//std::cout << RED << "Destructor called" << NC << std::endl;
+Fixed:: ~Fixed(void){
 }
 
 int Fixed::getRawBits( void ) const
@@ -46,14 +40,10 @@ void Fixed::setRawBits( int const raw )
 	this->_fixed = raw;
 }
 
-Fixed::Fixed(const int _intNew) : _fixed(_intNew << _fractional)
-{
-	//std::cout << GREEN << "Int constructor called" << NC << std::endl;
+Fixed::Fixed(const int _intNew) : _fixed(_intNew << _fractional){
 }
 
-Fixed::Fixed(const float _floatNew) : _fixed(roundf(_floatNew * (1 << _fractional)))
-{
-	//std::cout << GREEN << "Float constructor called" << NC << std::endl;
+Fixed::Fixed(const float _floatNew) : _fixed(roundf(_floatNew * (1 << _fractional))){
 }
 
 float Fixed::toFloat( void ) const
@@ -81,7 +71,7 @@ Fixed Fixed::operator+(const Fixed& _arg) const
 {
 	Fixed _result;
 
-	_result = this->_fixed + _arg._fixed;
+	_result.setRawBits(this->_fixed + _arg._fixed);
 	return (_result);
 }
 
@@ -89,7 +79,7 @@ Fixed Fixed::operator-(const Fixed& _arg) const
 {
 	Fixed _result;
 
-	_result = this->_fixed - _arg._fixed;
+	_result.setRawBits(this->_fixed - _arg._fixed);
 	return (_result);
 }
 
@@ -100,7 +90,6 @@ Fixed Fixed::operator*(const Fixed& _arg) const
 
 	_raw = (long long)this->_fixed * _arg._fixed;
 
-    // Scale the result back down by dividing by 2^8 (or shifting right by _fractional)
     // The result is already correctly scaled by 2^8, so we set the raw bits.
     _result.setRawBits(_raw >> _fractional); // SHIFT RIGHT by 8 bits
 	return (_result);
@@ -111,15 +100,12 @@ Fixed Fixed::operator/(const Fixed& _arg) const
 	Fixed _result;
 	long long _raw; //-> Helps to prevent overflow and store the result correctly
 
-	_raw = (long long)this->_fixed / _arg._fixed;
+	_raw = (long long)this->_fixed << _fractional; //Moving the bits of the number to maintain the presition point
 
     // Scale the result back down by dividing by 2^8 (or shifting right by _fractional)
-    // The result is already correctly scaled by 2^8, so we set the raw bits.
-    _result.setRawBits(_raw >> _fractional); // SHIFT RIGHT by 8 bits
+    _result.setRawBits(_raw / _arg._fixed); // SHIFT RIGHT by 8 bits
 	return (_result);
 }
-
-
 
 bool Fixed::operator>(const Fixed& _arg) const
 {
