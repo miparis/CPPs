fc3b7f839cb11e9305b960529deae92d412015df
miparis <miparis@student.42madrid.com>
2026-01-27 09:30:59 +0100

    cpp0s first version

---
+++ cpp03/ex00/ClapTrap.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miparis <miparis@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/27 09:17:13 by miparis           #+#    #+#             */
+/*   Updated: 2026/01/27 09:26:44 by miparis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap (std::string _name)
+{
+	
+}
+
+ClapTrap:: ~ClapTrap (void)
+{
+	
+}
+void attack(const std::string& target);
+void takeDamage(unsigned int amount);
+void beRepaired(unsigned int amount);
---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miparis <miparis@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/27 09:17:16 by miparis           #+#    #+#             */
+/*   Updated: 2026/01/27 09:24:04 by miparis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <iostream>
+#include <cmath>
+
+const std::string GREEN  = "\033[0;32m";
+const std::string YELLOW = "\033[0;33m";
+const std::string RED    = "\033[0;31m";
+const std::string BLUE   = "\033[0;34m";
+const std::string PURPLE = "\033[0;35m";
+const std::string BOLD   = "\033[1m";
+const std::string NC     = "\033[0m";
+
+class ClapTrap
+{
+
+	public:
+	ClapTrap(std::string _name);
+	~ClapTrap(void);
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+
+	private:
+	std::string _name; //, which is passed as a parameter to the constructor
+	int	_hitPoints; // (10), representing the health of the ClapTrap
+	int	_energyPoints;//  (10)
+	int	_attackDamage; // (0)
+};
+
+#endif
