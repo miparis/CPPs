00690871349c0b40289315978e871ebdf70718b3
miparis <miparis@student.42madrid.com>
2026-01-28 09:50:34 +0100

    Avance ex00

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,21 +6,53 @@
 /*   By: miparis <miparis@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 09:17:13 by miparis           #+#    #+#             */
-/*   Updated: 2026/01/27 09:26:44 by miparis          ###   ########.fr       */
+/*   Updated: 2026/01/28 09:50:06 by miparis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap (std::string _name)
-{
-	
+ClapTrap::ClapTrap (std::string _name): _name(_name), _hitPoints(10), _energyPoints(10), _attackDamage(0){
+	std::cout << PURPLE << "ClapTrap" << this->_name << " ready!" << NC << std::endl;
+	std::cout << GREEN << "Hit = " << this->_hitPoints << NC << std::endl;
+	std::cout << YELLOW << "Energy = " << this->_energyPoints << NC << std::endl;
+	std::cout << RED << "Damage = " << this->_attackDamage << NC << std::endl;
 }
 
 ClapTrap:: ~ClapTrap (void)
 {
+	std::cout << PURPLE << "ClapTrap" << this->_name << ": GAME OVER" << NC << std::endl;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (this->_energyPoints >= 1)
+	{
+		std::cout << PURPLE << "ClapTrap" << this->_name << " attacks " << target << " causing 1 points of damage!" << NC << std::endl;
+		this->_attackDamage++;
+		this->_energyPoints--;
+	}
+	else
+		std::cout << PURPLE << "ClapTrap" << this->_name << " No energy to attack left!" << NC << std::endl;
+}
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (this->_energyPoints >= amount)
+	{
+		std::cout << PURPLE << "ClapTrap" << this->_name << " receives " << amount << " of damage!" << NC << std::endl;
+		this->_hitPoints--;
+	}
+	else
+		std::cout << PURPLE << "ClapTrap" << this->_name << " No hit points left!" << NC << std::endl;
 	
 }
-void attack(const std::string& target);
-void takeDamage(unsigned int amount);
-void beRepaired(unsigned int amount);
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->_energyPoints >= 1)
+	{
+		std::cout << PURPLE << "ClapTrap" << this->_name << " repairs itself by " << amount << " points!" << NC << std::endl;
+		this->_hitPoints--;
+	}
+	else
+		std::cout << PURPLE << "ClapTrap" << this->_name << " No energy points left!" << NC << std::endl;
+}
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,7 +6,21 @@
 /*   By: miparis <miparis@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 09:17:19 by miparis           #+#    #+#             */
-/*   Updated: 2026/01/27 09:17:20 by miparis          ###   ########.fr       */
+/*   Updated: 2026/01/28 09:48:41 by miparis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+int main(void)
+{
+	ClapTrap _mario("_Mario");
+	ClapTrap _warrio("_Warrio");
+
+	std::cout << GREEN <<  "Battle begins!" << NC << std::endl;
+	_mario.attack("_Warrio");
+	_warrio.takeDamage(5);
+	_warrio.beRepaired(2);
+	_warrio.attack("_Mario");
+	_mario.takeDamage(4);
+}
