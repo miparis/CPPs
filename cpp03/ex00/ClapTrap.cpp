9a19ad75bf723fd9f31273c76910854f4d7a5c63
miparis <miparis@student.42madrid.com>
2026-01-28 17:39:38 +0100

    ex00 done

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: miparis <miparis@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 09:17:13 by miparis           #+#    #+#             */
-/*   Updated: 2026/01/28 09:56:43 by miparis          ###   ########.fr       */
+/*   Updated: 2026/01/28 17:33:25 by miparis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,43 +21,43 @@ ClapTrap::ClapTrap (std::string _name): _name(_name), _hitPoints(10), _energyPoi
 
 ClapTrap:: ~ClapTrap (void)
 {
-	std::cout << PURPLE << "ClapTrap" << this->_name << ": GAME OVER" << NC << std::endl;
+	std::cout << PURPLE << "ClapTrap " << this->_name << ": GAME OVER" << NC << std::endl;
 }
 
 void ClapTrap::attack(const std::string& target)
 {
-	if (this->_energyPoints >= 1)
+	if (this->_energyPoints > 0 && this->_hitPoints > 0)
 	{
-		std::cout << PURPLE << "ClapTrap" << this->_name << " attacks " << target << " causing 1 points of damage!" << NC << std::endl;
-		this->_attackDamage++;
+		std::cout << BLUE << "ClapTrap " << this->_name << " attacks " << target << NC << std::endl;
 		this->_energyPoints--;
 	}
 	else
-		std::cout << PURPLE << "ClapTrap" << this->_name << " No energy to attack left!" << NC << std::endl;
+		std::cout << YELLOW << "ClapTrap " << this->_name << " cant attack! No energy or hit points left!" << NC << std::endl;
 }
 void ClapTrap::takeDamage(unsigned int amount)
 {
-	if (this->_energyPoints >= amount)
+	if (this->_hitPoints >= amount)
 	{
-		std::cout << PURPLE << "ClapTrap" << this->_name << " receives " << amount << " of damage!" << NC << std::endl;
-		this->_hitPoints--;
+		std::cout << RED << "ClapTrap " << this->_name << " receives " << amount << " of damage!" << NC << std::endl;
+		this->_hitPoints -= amount;
 	}
 	else
-		std::cout << PURPLE << "ClapTrap" << this->_name << " No hit points left!" << NC << std::endl;
-	
+		std::cout << YELLOW << "ClapTrap " << this->_name << " No hit points left to keep standing! Hit points = " << this->_hitPoints << NC << std::endl;
 }
 void ClapTrap::beRepaired(unsigned int amount)
 {
-	if (this->_energyPoints >= 1)
+	if (this->_energyPoints > 0 && this->_hitPoints > 0)
 	{
-		std::cout << PURPLE << "ClapTrap" << this->_name << " repairs itself by " << amount << " points!" << NC << std::endl;
-		this->_hitPoints--;
+		std::cout << GREEN << "ClapTrap " << this->_name << " repairs itself by " << amount << " points!" << NC << std::endl;
+		this->_hitPoints += amount;
+		this->_energyPoints--;
 	}
 	else
-		std::cout << PURPLE << "ClapTrap" << this->_name << " No energy points left!" << NC << std::endl;
+		std::cout << YELLOW << "ClapTrap " << this->_name << " has no energy points left to repair itself! To repair = " << amount << " Energy Points = " << _energyPoints << NC << std::endl;
 }
 
-void ClapTrap::status(void) const {
+void ClapTrap::status(void) const
+{
     std::cout << "\n" << "--- [ STATUS: " << PURPLE << _name << NC << " ] ---" << std::endl;
     std::cout << "| " << GREEN  << "HP: " << _hitPoints << NC;
     std::cout << " | " << YELLOW << "EP: " << _energyPoints << NC;
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,52 +6,39 @@
 /*   By: miparis <miparis@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 09:17:19 by miparis           #+#    #+#             */
-/*   Updated: 2026/01/28 09:57:34 by miparis          ###   ########.fr       */
+/*   Updated: 2026/01/28 17:32:05 by miparis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-/*int main(void)
+int main()
 {
-	ClapTrap _mario("_Mario");
-	ClapTrap _warrio("_Warrio");
+	std::cout << "--- Create ---" << std::endl;
+	ClapTrap mario("Mario");
+	ClapTrap warrio("Warrio");
+	mario.status();
+	warrio.status();
 
-	std::cout << GREEN <<  "Battle begins!" << NC << std::endl;
-	_mario.attack("_Warrio");
-	_warrio.takeDamage(5);
-	_warrio.beRepaired(2);
-	_warrio.attack("_Mario");
-	_mario.takeDamage(4);
-}*/
-int main() {
-    std::cout << "--- TEST 1: Creación ---" << std::endl;
-    ClapTrap mario("Mario");
-    ClapTrap warrio("Warrio");
-    mario.status();
-    warrio.status();
+	std::cout << "---  Use all energy ---" << std::endl;
+	for (int i = 0; i < 10; i++)
+		mario.attack("a Goomba");
+	mario.status();
 
-    std::cout << "--- TEST 2: Agotar Energía ---" << std::endl;
-    // Mario tiene 10 EP. Atacamos 10 veces.
-    for (int i = 0; i < 10; i++) {
-        mario.attack("un Goomba");
-    }
-    mario.status(); // Aquí verás EP en 0
+	std::cout << "--- Failed actions ---" << std::endl;
+	mario.attack("a Goomba");
+	mario.beRepaired(5);
+	mario.status();
 
-    std::cout << "--- TEST 3 y 4: Intentos fallidos ---" << std::endl;
-    mario.attack("un Goomba"); // No debería gastar nada ni hacer nada
-    mario.beRepaired(5);       // No debería curar nada
-    mario.status();            // Los valores deberían seguir igual (EP 0, HP 10)
+	std::cout << "--- Dead because of points ---" << std::endl;
+	warrio.status();
+	warrio.takeDamage(10);
+	warrio.status();
 
-    std::cout << "--- TEST 5: Muerte por daño ---" << std::endl;
-    warrio.status();           // Estado inicial de Warrio
-    warrio.takeDamage(10);     // Recibe daño letal
-    warrio.status();           // Verás HP en 0
+	std::cout << "--- Post-mortem ---" << std::endl;
+	warrio.attack("Mario");
+	warrio.beRepaired(10);
+	warrio.status();
 
-    std::cout << "--- TEST 6: Acciones post-mortem ---" << std::endl;
-    warrio.attack("Mario");
-    warrio.beRepaired(10);
-    warrio.status();           // Los valores no deben cambiar (HP sigue en 0)
-
-    return 0;
+	return 0;
 }
