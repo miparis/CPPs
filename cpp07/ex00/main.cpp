9a1266d2887e348c9b9b302dc125e30b4f55bff1
miparis <miparis@student.42madrid.com>
2026-07-06 12:04:52 +0200

    ex00

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miparis <miparis@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/06 11:32:51 by miparis           #+#    #+#             */
+/*   Updated: 2026/07/06 12:04:12 by miparis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main( void )
+{
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miparis <miparis@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/06 11:32:48 by miparis           #+#    #+#             */
+/*   Updated: 2026/07/06 12:02:28 by miparis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+const std::string GREEN  = "\033[0;32m";
+const std::string YELLOW = "\033[0;33m";
+const std::string RED    = "\033[0;31m";
+const std::string BLUE   = "\033[0;34m";
+const std::string PURPLE = "\033[0;35m";
+const std::string BOLD   = "\033[1m";
+const std::string NC     = "\033[0m";
+
+template <typename T>
+void swap(T& firstArg, T& secondArg)
+{
+	T aux = firstArg;
+	firstArg = secondArg;
+	secondArg = aux;
+}
+template <typename T>
+T min(T firstArg, T secondArg)
+{
+	if (firstArg < secondArg)
+		return (firstArg);
+	else if (secondArg < firstArg)
+		return (secondArg);
+	return (secondArg);
+}
+template <typename T>
+T max(T firstArg,T secondArg)
+{
+	if (firstArg > secondArg)
+		return (firstArg);
+	else if (secondArg > firstArg)
+		return (secondArg);
+	return (secondArg);	
+}
